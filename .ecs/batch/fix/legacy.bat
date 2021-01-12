1ef9932b5c6cd9c9fd65e4a62e7eb69a0accb4c7
Marko Cupic <m.cupic@gmx.ch>
2021-01-12 19:43:43 +0100

    bugfix

--- .ecs/batch/fix/legacy.bat
+++ .ecs/batch/fix/legacy.bat
@@ -6,7 +6,5 @@ cd..
 cd..
 cd..
 cd..
-:: legacy
 vendor\bin\ecs check vendor/markocupic/contao-crm-bundle/src/Resources/contao --fix --config vendor/markocupic/contao-crm-bundle/.ecs/config/legacy.php
-::
 cd vendor/markocupic/contao-crm-bundle/.ecs./batch/fix
